0cd8e33cfb338883a66f902b983f9fa18de12ead
MuskanRathore <81912010+MuskanRathore@users.noreply.github.com>
2022-01-22 15:33:41 +0530

    Add files via upload

---
+++ data/products.js
@@ -0,0 +1,76 @@
+const products = [
+  {
+    name: 'Tablets',
+    image: '/images/Tablet.jpg',
+    description:
+      ' A substance or preparation used in treating disease.',
+    brand: 'Medicine',
+    category: 'Tablet',
+    price: 9.99,
+    countInStock: 3,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Syrups',
+    image: '/images/Syrup.jpg',
+    description:
+      'A syrup is a concentrated or nearly saturated solution of sucrose in water.',
+    brand: 'Syrup',
+    category: 'Syrup',
+    price: 59.99,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Thermometer',
+    image: '/images/Thermometer.jpg',
+    description:
+      'An instrument for measuring and indicating temperature, typically one consisting of a narrow, hermetically sealed glass tube marked with graduations and having at one end a bulb containing mercury or alcohol which extends along the tube as it expands.',
+    brand: 'ThermoMeter',
+    category: 'Electronics',
+    price: 92.99,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Paracetamol',
+    image: '/images/Capsule.jpg',
+    description:
+      'A synthetic compound used as a drug to relieve and reduce fever, usually taken in tablet form.',
+    brand: 'Paracetamol',
+    category: 'Tablet',
+    price: 99.99,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Nicozar',
+    image: '/images/Nicozar.jpg',
+    description:
+      'Nicoz 1mg Tablet is used in the treatment of Blood clots.',
+    brand: 'Logitech',
+    category: 'Tablet',
+    price: 49.99,
+    countInStock: 7,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Ointment',
+    image: '/images/Ointment.jpg',
+    description:
+      'A translucent jelly consisting of a mixture of hydrocarbons, used as a lubricant or ointment.',
+    brand: 'American brand',
+    category: 'Ointment',
+    price: 29.99,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+]
+
+export default products
